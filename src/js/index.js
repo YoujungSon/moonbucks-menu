13f31128c6c59478390f1dfcff6307e3a8deb864
YoujungSon <andn1026@gmail.com>
2023-01-21 17:05:21 +0900

    7. 메뉴 추가하기 - 사용자가 입력한 값을 메뉴 리스트에 추가하기 | DOM 핸들링

--- src/js/index.js
+++ src/js/index.js
@@ -8,15 +8,35 @@
 // - [] 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
 // - [] 사용자 입력값이 빈 값이라면 추가되지 않는다.
 
+const $ = (selector) => document.querySelector(selector);
+
 function App() {
   // form태그가 자동으로 전송되는것을 막아준다.
-  document.querySelector('#espresso-menu-form').addEventListener('submit', (e) => {
+  $('#espresso-menu-form').addEventListener('submit', (e) => {
     e.preventDefault();
   });
   // 메뉴 이름 입력 받기
-  document.querySelector('#espresso-menu-name').addEventListener('keypress', (e) => {
+  $('#espresso-menu-name').addEventListener('keypress', (e) => {
     if (e.key === 'Enter') {
-      console.log(document.querySelector('#espresso-menu-name').value);
+      const espressoMenuName = $('#espresso-menu-name').value;
+      const menuItemTemplate = (espressoMenuName) => {
+        return `<li class="menu-list-item d-flex items-center py-2">
+      <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
+      <button
+        type="button"
+        class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+      >
+        수정
+      </button>
+      <button
+        type="button"
+        class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+      >
+        삭제
+      </button>
+    </li>`;
+      };
+      $('#espresso-menu-list').insertAdjacentHTML('beforeend', menuItemTemplate(espressoMenuName));
     }
   });
 }
